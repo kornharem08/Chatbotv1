7a2eaef53984949d79e8e6323f9d701b82541ee5
tanakorn <tana_korn01@hotmail.com>
2019-05-28 22:40:09 +0700

    test

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.get("/ok", function (req, res) {
 
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === config.FB_VERIFY_TOKEN) {
+      req.query['hub.verify_token'] === 'user-hook') {
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
