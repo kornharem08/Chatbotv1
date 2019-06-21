61e73f1440dafffbd5d637a4325c07a3c6e93c40
soapsabee <s5930213055@phuket.psu.ac.th>
2019-06-21 14:02:26 +0700

    test api export

---
+++ helper/api.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+const config = require("../config");
+
+
+
+
+module.exports = {
+    callSendAPI
+}
+
+
+
+const callSendAPI = async (messageData) => {
+
+    const url = "https://graph.facebook.com/v3.0/me/messages?access_token=" + config.FB_PAGE_TOKEN;
+    await axios.post(url, messageData)
+      .then(function (response) {
+        if (response.status == 200) {
+          var recipientId = response.data.recipient_id;
+          var messageId = response.data.message_id;
+         // information(recipientId);
+          if (messageId) {
+            console.log(
+              "Successfully sent message with id %s to recipient %s",
+              messageId,
+              recipientId
+            );
+          } else {
+            console.log(
+              "Successfully called Send API for recipient %s",
+              recipientId
+            );
+          }
+        }
+      })
+      .catch(function (error) {
+        console.log(error.response.headers);
+      });
+  }
