74e7c90edfa73e9a56faa923e4f9601d5833b75c
tanakorn <tana_korn01@hotmail.com>
2019-06-26 15:39:07 +0700

    edit some

--- src/views/function.js
+++ src/views/function.js
@@ -2,7 +2,7 @@ const api = require('../helper/api.js')
 const fb = require("../helper/fbTemplate");
 const Dict = require("../helper/dictionary");
 
-const mainmenu = async (sender) => {
+const mainmenu = (sender) => {
    
    let elementsMenu = [
         fb.carouselTemplate("Welcome!","https://www.computing.psu.ac.th/th/wp-content/uploads/2018/03/PSU_CoC_ENG.png","We have the right hat for everyone.",[fb.buttonsURL(urlweb.sisurl,"View Website"), fb.buttonsURL(urlweb.sisurl,"Schedule"),fb.buttonsURL(urlweb.sisurl,"Test")]),
