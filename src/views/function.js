9c978fa7cdbcf4fc90a603f3b352c722dce0d06e
tanakorn <tana_korn01@hotmail.com>
2019-06-26 15:35:16 +0700

    edit some

--- src/views/function.js
+++ src/views/function.js
@@ -4,7 +4,7 @@ const Dict = require("../helper/dictionary");
 
 const mainmenu = async (sender) => {
    
-   elements = Menu[
+   let elementsMenu = [
         fb.carouselTemplate("Welcome!","https://www.computing.psu.ac.th/th/wp-content/uploads/2018/03/PSU_CoC_ENG.png","We have the right hat for everyone.",[fb.buttonsURL(urlweb.sisurl,"View Website"), fb.buttonsURL(urlweb.sisurl,"Schedule"),fb.buttonsURL(urlweb.sisurl,"Test")]),
         fb.carouselTemplate("Welcome!","https://www.computing.psu.ac.th/th/wp-content/uploads/2018/03/PSU_CoC_ENG.png","We have the right hat for everyone.",[fb.buttonsURL(urlweb.sisurl,"View Website"), fb.buttonsURL(urlweb.sisurl,"Test")])
         // fbTemplate.carouselTemplate("Welcome!","https://www.computing.psu.ac.th/th/wp-content/uploads/2018/03/PSU_CoC_ENG.png","We have the right hat for everyone.",fbTemplate.buttonsTemplate(urlweb.sisurl,"Schedule"))
