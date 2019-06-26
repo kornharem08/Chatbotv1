c0c081031955bdd2bf2db0f70168e4a1bea39351
tanakorn <tana_korn01@hotmail.com>
2019-06-26 15:20:30 +0700

    edit some

--- src/handleAPI/aiAction.js
+++ src/handleAPI/aiAction.js
@@ -29,10 +29,10 @@ const handleApiAiAction = async (
     //   // //   "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/881e6651881085.58fd911b65d88.png";
     //   // // sendImageMessage(sender, imgUrl);
     //   break;
-    // case "send-music":
-    //   // var responseText = "The toys";
-    //   // sendTextMessage(sender, responseText);
-    //   break;
+     case "send-music":
+         var responseText = "The toys";
+        func.sendTextMessage(sender, responseText);
+       break;
     // case "send-quick-reply":
     //   // var replies = fb.quickreplyTemplate("test",[fb.quickreply("test","test",null),fb.quickreply("test","test",null),fb.quickreply("test","test",null)])
     //   // sendQuickReply(sender, replies);
@@ -48,41 +48,41 @@ const handleApiAiAction = async (
 };
 
 
-async function handleCardMessages(messages, sender) { /// < เป็นตัวอย่างสำหรับทำฟังก์ชั่นใช้ซํ้าที่น่าเลียนแบบอยู่เหมือนกัน เพราะก่อนหน้านี้สมองเบลอๆยังไม่รู้ว่ามันใช้ทำอะไร
-  let elements = [];
-  for (var m = 0; m < messages.length; m++) {
-    let message = messages[m];
-    let buttons = [];
-    for (var b = 0; b < message.buttons.length; b++) {
-      let isLink = message.buttons[b].postback.substring(0, 4) === "http";
-      let button;
-      if (isLink) {
-        button = {
-          type: "web_url",
-          title: message.buttons[b].text,
-          url: message.buttons[b].postback,
-          webview_height_ratio: full,
-          messenger_extensions: true
-        };
-      } else {
-        button = {
-          type: "postback",
-          title: message.buttons[b].text,
-          payload: message.buttons[b].postback
-        };
-      }
-      buttons.push(button);
-    }
-    let element = {
-      title: message.title,
-      image_url: message.imageUrl,
-      subtitle: message.subtitle,
-      buttons: buttons
-    };
-    elements.push(element);
-  }
-  await sendGenericMessage(sender, elements);
-}
+// async function handleCardMessages(messages, sender) { /// < เป็นตัวอย่างสำหรับทำฟังก์ชั่นใช้ซํ้าที่น่าเลียนแบบอยู่เหมือนกัน เพราะก่อนหน้านี้สมองเบลอๆยังไม่รู้ว่ามันใช้ทำอะไร
+//   let elements = [];
+//   for (var m = 0; m < messages.length; m++) {
+//     let message = messages[m];
+//     let buttons = [];
+//     for (var b = 0; b < message.buttons.length; b++) {
+//       let isLink = message.buttons[b].postback.substring(0, 4) === "http";
+//       let button;
+//       if (isLink) {
+//         button = {
+//           type: "web_url",
+//           title: message.buttons[b].text,
+//           url: message.buttons[b].postback,
+//           webview_height_ratio: full,
+//           messenger_extensions: true
+//         };
+//       } else {
+//         button = {
+//           type: "postback",
+//           title: message.buttons[b].text,
+//           payload: message.buttons[b].postback
+//         };
+//       }
+//       buttons.push(button);
+//     }
+//     let element = {
+//       title: message.title,
+//       image_url: message.imageUrl,
+//       subtitle: message.subtitle,
+//       buttons: buttons
+//     };
+//     elements.push(element);
+//   }
+//   await sendGenericMessage(sender, elements);
+// }
 
 // const sendGenericMessage = async (recipientId, elements) => {
 //   var messageData = {
