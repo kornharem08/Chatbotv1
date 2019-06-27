45068f71548f14e13c63d5ef88e65fd0298a24ad
tanakorn <tana_korn01@hotmail.com>
2019-06-27 18:43:07 +0700

    save

--- src/views/function.js
+++ src/views/function.js
@@ -30,7 +30,7 @@ const btnMessage = async (sender) => {
 }
 const Messageinfo = async (sender) => {
    
-  let Message = "------Name------\nTanakorn Pitakchaichan\n======================\n------Email-----\nTana_korn01@hotmail.com\n======================\n---Phone number---\n090-9858754\n======================\n--Advisor Name--\nKorawit Prutsachainimmit\n======================"
+  let Message = "         Name         \nTanakorn Pitakchaichan\n======================\n         Email        \nTana_korn01@hotmail.com\n======================\n     Phone number     \n090-9858754\n======================\n     Advisor Name     \nKorawit Prutsachainimmit\n======================"
   
     
   
