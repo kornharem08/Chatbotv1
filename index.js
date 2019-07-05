bf8a1c090b6741265352ab6cff64b41166d9cf27
soapsabee <s5930213055@phuket.psu.ac.th>
2019-07-05 23:43:20 +0700

    'implement_cors'

--- index.js
+++ index.js
@@ -11,14 +11,30 @@ const handle = require("./src/handleAPI/aiAction");
 const handlePb = require("./src/handleAPI/aiPostback");
 const func = require("./src/views/function.js");
 const notification = require("./src/helper/notification");
+const cors = require('cors');
 //const assets = require("./src/assets");
 
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
-
+var allowedOrigins = ['http://localhost:8080',
+                      'http://yourapp.com'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 //app.listen(3000);
 // function setupGetStartedButton(res) {
