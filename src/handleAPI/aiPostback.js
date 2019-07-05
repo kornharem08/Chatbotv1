a704d62a53f79e712a09b38e4e060308c9734a19
soapsabee <s5930213055@phuket.psu.ac.th>
2019-07-05 18:10:37 +0700

    'test-notification'

--- src/handleAPI/aiPostback.js
+++ src/handleAPI/aiPostback.js
@@ -1,17 +1,46 @@
 const func = require("../views/function.js");
 
-const handleAiPostback = async(
+const handleAiPostback = async (
 
     sender,
     payload
 
 ) => {
 
-    switch(payload) {
+    switch (payload) {
 
         case "MainMenu_Payload":
             func.mainmenu(sender);
             break;
+        case "Student-information":
+            func.btnMessage(sender);
+            break;
+        case "Personal-information":
+            func.Messageinfo(sender);
+            break;
+        case "Grade&GPA":
+            func.messageGradeGPA(sender);
+            break;
+        case "Class&Examination-Schedule":
+            func.btnMessageclassEx(sender);
+            break;
+        case "Examination-Schedule":
+            func.messageExSc(sender);
+            break;
+        case "Class-schedule":
+            func.messageClassSc(sender);
+            break;
+        case "Edit-Student-information":
+            func.messageEditinfo(sender);
+            break;
+            case "Exam-notification-day":
+            func.MessageExamnotif(sender);
+            break;
+            case "Cancle-Exam-notification-day":
+            func.MessageCancleExamnotif(sender);
+            break;
+
+
 
         default:
 
@@ -21,7 +50,31 @@ const handleAiPostback = async(
 
 }
 
+<<<<<<< HEAD
   module.exports = {
+=======
+
+// // const sendGenericMessage = async (recipientId, elements) => {
+// //     var messageData = {
+// //       recipient: {
+// //         id: recipientId
+// //       },
+// //       message: {
+// //         attachment: {
+// //           type: "template",
+// //           payload: {
+// //             template_type: "generic",
+// //             elements: elements
+// //           }
+// //         }
+// //       }
+// //     };
+// //     await api.callSendAPI(messageData);
+// //   };
+
+
+module.exports = {
+>>>>>>> 415d09fb316bce182d1020c998594e15453a971a
     handleAiPostback
-    
-  }
+
+}
--- src/views/function.js
+++ src/views/function.js
@@ -3,98 +3,187 @@ const fb = require("../helper/fbTemplate");
 const Dict = require("../helper/dictionary");
 const urlweb = require("../helper/webview.js");
 
-const mainmenu =(sender) => {
-   
-   let elementsMenu = [
-        fb.carouselTemplate("Welcome!","https://www.computing.psu.ac.th/th/wp-content/uploads/2018/03/PSU_CoC_ENG.png","We have the right hat for everyone.",[fb.buttonsURL(urlweb.sisurl,"View Website"), fb.buttonsURL(urlweb.sisurl,"Schedule"),fb.buttonsURL(urlweb.sisurl,"Test")]),
-        fb.carouselTemplate("Welcome!","https://www.computing.psu.ac.th/th/wp-content/uploads/2018/03/PSU_CoC_ENG.png","We have the right hat for everyone.",[fb.buttonsURL(urlweb.sisurl,"View Website"), fb.buttonsURL(urlweb.sisurl,"Test")])
-        // fbTemplate.carouselTemplate("Welcome!","https://www.computing.psu.ac.th/th/wp-content/uploads/2018/03/PSU_CoC_ENG.png","We have the right hat for everyone.",fbTemplate.buttonsTemplate(urlweb.sisurl,"Schedule"))
-      ];
+const mainmenu = async (sender) => {
 
-      sendGenericMessage(sender,elementsMenu)
+  let elementsMenu = [
+    fb.carouselTemplate("Welcome!", "https://www.phuket.psu.ac.th/wp-content/uploads/2019/03/cropped-PSU_PHUKET-EN.png", "We have the right hat for everyone.", [fb.buttons("Student information", "Student-information"), fb.buttons("Grade & GPA", "Grade&GPA"), fb.buttons("Class&Examination Schedule", "Class&Examination-Schedule")]),
+    fb.carouselTemplate("Welcome!", "https://www.phuket.psu.ac.th/wp-content/uploads/2019/03/cropped-PSU_PHUKET-EN.png", "We have the right hat for everyone.", [fb.buttons("Exam notification day", "Exam-notification-day"), fb.buttonsURL(urlweb.sisurl, "Academic calendar for undergraduate"),fb.buttons("Cancle Exam notification day","Cancle-Exam-notification-day")]),
+
+  ];
+
+  let replies = fb.quickreplyTemplate("", [fb.quickreply("TH", "test", null), fb.quickreply("ENG", "test", null)])
+
+  await sendGenericMessage(sender, elementsMenu)
+  await sendQuickReply(sender, replies);
+
+}
+const btnMessage = async (sender) => {
+
+  let btnMessage = fb.buttonsTemplate("What do you want to do next?", [fb.buttons("Personal information", "Personal-information"), fb.buttons("Edit Personal information", "Edit-Student-information"), fb.buttons("Back", "back")])
+
+
+
+
+  await sendBtnMessage(sender, btnMessage)
+
+
+}
+const Messageinfo = async (sender) => {
+
+  let Message = "Name\nTanakorn Pitakchaichan\n======================\nEmail\nTana_korn01@hotmail.com\n======================\nPhone number\n090-9858754\n======================\nAdvisor Name\nKorawit Prutsachainimmit\n======================"
+
+  await sendTextMessage(sender, Message)
+
+
+}
+const messageGradeGPA = async (sender) => {
+
+  let replies = fb.quickreplyTemplate("Please select Semester/Year criteria", [fb.quickreply("<< Back", "test", null), fb.quickreply("1/2559", "test", null), fb.quickreply("2/2559", "test", null), fb.quickreply("1/2560", "test", null), fb.quickreply("2/2560", "test", null), fb.quickreply("1/2561", "test", null), fb.quickreply("2/2561", "test", null)])
+  await sendQuickReply(sender, replies);
+
+}
+
+const btnMessageclassEx = async (sender) => {
+
+  let btnMessage = fb.buttonsTemplate("What do you want to do next?", [fb.buttons("Class Schedule", "Class-schedule"), fb.buttons("Examination Schedule", "Examination-Schedule"), fb.buttons("Back", "back")])
+
+  await sendBtnMessage(sender, btnMessage)
+
+}
+
+const messageExSc = async (sender) => {
+
+  let replies = fb.quickreplyTemplate("Please select Semester/Year criteria", [fb.quickreply("<< Back", "test", null), fb.quickreply("1/2559", "test", null), fb.quickreply("2/2559", "test", null), fb.quickreply("1/2560", "test", null), fb.quickreply("2/2560", "test", null), fb.quickreply("1/2561", "test", null), fb.quickreply("2/2561", "test", null)])
+  await sendQuickReply(sender, replies);
+
+
+}
+const messageClassSc = async (sender) => {
+
+  let replies = fb.quickreplyTemplate("Please select Semester/Year criteria", [fb.quickreply("<< Back", "test", null), fb.quickreply("1/2559", "test", null), fb.quickreply("2/2559", "test", null), fb.quickreply("1/2560", "test", null), fb.quickreply("2/2560", "test", null), fb.quickreply("1/2561", "test", null), fb.quickreply("2/2561", "test", null)])
+  await sendQuickReply(sender, replies);
+
+
+}
+
+const messageEditinfo = async (sender) => {
+  let Message = "Name\nTanakorn Pitakchaichan\n======================\nPhone number\n090-252812\n======================"
+
+  let replies = fb.quickreplyTemplate("Please select Menu Edit", [fb.quickreply("<< Back", "test", null), fb.quickreply("Email", "test", null), fb.quickreply("Phone number", "test", null)])
+
+  await sendTextMessage(sender, Message)
+  await sendQuickReply(sender, replies);
+
+
+}
+
+const MessageExamnotif = async (sender) => {
+
+  let Message = "Exam notification day Successfully"
+
+  await sendTextMessage(sender, Message)
+
+}
+
+const MessageCancleExamnotif = async (sender) => {
+
+  let Message = "Cancle Exam notification day Successfully"
+
+  await sendTextMessage(sender, Message)
+
 
 }
 
 
 
 const sendGenericMessage = async (recipientId, elements) => {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        attachment: {
-          type: "template",
-          payload: {
-            template_type: "generic",
-            elements: elements
-          }
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: elements
         }
       }
-    };
-    await api.callSendAPI(messageData);
+    }
   };
-  const sendTextMessage = async (recipientId, text) => {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        text: text
-      }
-    };
-    await api.callSendAPI(messageData);
+  await api.callSendAPI(messageData);
+};
+const sendTextMessage = async (recipientId, text) => {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: text
+    }
   };
-  const sendImageMessage = async (recipientId, imageUrl) => {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        attachment: {
-          type: "image",
-          payload: {
-            url: imageUrl
-          }
+  await api.callSendAPI(messageData);
+};
+const sendImageMessage = async (recipientId, imageUrl) => {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "image",
+        payload: {
+          url: imageUrl
         }
       }
-    };
-    await api.callSendAPI(messageData);
+    }
   };
-  const sendQuickReply = async (recipientId, message) => { // อันนี้ quickReply
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: message
-    };
-    await api.callSendAPI(messageData);
+  await api.callSendAPI(messageData);
+};
+const sendQuickReply = async (recipientId, message) => { // อันนี้ quickReply
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: message
   };
-  
-  const sendBtnMessage = async (recipientId, payload) =>{  /// https://developers.facebook.com/docs/messenger-platform/send-messages/template/button
-  
-    var messageData = {
-      recipient:{
-        id: recipientId
-      },
-      message:{
-        attachment:{
-          type:"template",
-          payload:payload
-        }
+  await api.callSendAPI(messageData);
+};
+
+const sendBtnMessage = async (recipientId, payload) => {  /// https://developers.facebook.com/docs/messenger-platform/send-messages/template/button
+
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload
       }
-    };
-    await api.callSendAPI(messageData);
-  }
+    }
+  };
+  await api.callSendAPI(messageData);
+}
 
 
-  
+
 module.exports = {
-    sendGenericMessage,
-    sendTextMessage,
-    sendImageMessage,
-    sendQuickReply,
-    sendBtnMessage,
-    mainmenu
+  sendGenericMessage,
+  sendTextMessage,
+  sendImageMessage,
+  sendQuickReply,
+  sendBtnMessage,
+  mainmenu,
+  btnMessage,
+  Messageinfo,
+  messageGradeGPA,
+  btnMessageclassEx,
+  messageExSc,
+  messageClassSc,
+  messageEditinfo,
+  MessageExamnotif,
+  MessageCancleExamnotif
+
 }
 
