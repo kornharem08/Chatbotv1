2be34e2eee0c39edfb044ac5e7eb45afa81a55cf
soapsabee <s5930213055@phuket.psu.ac.th>
2019-07-05 18:08:58 +0700

    'test-notification'

--- src/handleAPI/aiPostback.js
+++ src/handleAPI/aiPostback.js
@@ -21,26 +21,6 @@ const handleAiPostback = async(
 
 }
 
-
-// // const sendGenericMessage = async (recipientId, elements) => {
-// //     var messageData = {
-// //       recipient: {
-// //         id: recipientId
-// //       },
-// //       message: {
-// //         attachment: {
-// //           type: "template",
-// //           payload: {
-// //             template_type: "generic",
-// //             elements: elements
-// //           }
-// //         }
-// //       }
-// //     };
-// //     await api.callSendAPI(messageData);
-// //   };
-
-
   module.exports = {
     handleAiPostback
     
---
+++ src/helper/notification.js
@@ -0,0 +1,5 @@
+const func = require("../views/function.js");
+
+setInterval(function() {
+    func.sendTextMessage("2797221146971020","this is notification")
+}, 300000);
