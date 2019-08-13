87e9a4f25c03ac7c3413d251329b052e6ce21ded
soapsabee <rattana152513@gmail.com>
2019-08-13 22:55:48 +0700

    testRedis

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ redis.sadd("set", redisObj);
 // redis.sadd("set", redisObj2);
 redis.smembers("set", function(err, result) {
   
-  console.log("resule:"+result)
+  console.log("result:"+JSON.stringify(result))
   
 });
 redis.del("set");
