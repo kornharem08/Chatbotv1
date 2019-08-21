5875a9a20ebb7f2d4f3777e0d74c087727c678d4
soapsabee <rattana152513@gmail.com>
2019-08-21 17:41:04 +0700

    test

--- src/views/function.js
+++ src/views/function.js
@@ -67,13 +67,12 @@ const btnGradeGPAWebview = async (sender) => {
   let term  
  await redis.get(`${sender}`, function(err,result) {  
     
-    let data = JSON.parse(result)
-    
-    term = data.term
+    let data =result
+    console.log("data:"+data)
     
 });
-  let btnMessage = await fb.buttonsTemplate("Click to view Grade/GPA", [ fb.buttonsURL(`${urlweb.sisurl_grade}/5930213034/2561/2/${term}`, "View Grade/GPA"), fb.buttons("Back", "MainMenu_Payload")])
-  await sendBtnMessage(sender, btnMessage)
+ // let btnMessage = await fb.buttonsTemplate("Click to view Grade/GPA", [ fb.buttonsURL(`${urlweb.sisurl_grade}/5930213034/2561/2/${term}`, "View Grade/GPA"), fb.buttons("Back", "MainMenu_Payload")])
+ // await sendBtnMessage(sender, btnMessage)
 }
 
 const messageExSc = async (sender) => {
