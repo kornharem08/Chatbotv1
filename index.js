16e46f692fbec12d4d7bc61b9ececc5bb93a0706
soapsabee <rattana152513@gmail.com>
2019-08-23 23:35:06 +0700

    test_notihook

--- index.js
+++ index.js
@@ -79,26 +79,6 @@ app.use(cors({
   }
 }));
 
-const client = new google.auth.JWT(
-
-  keys.client_email,
-  null,
-  keys.private_key,
-  ['https://www.googleapis.com/auth/spreadsheets']
-
-);
-////////////////////////////////////// ส่วนของ Google Sheet
-client.authorize(function(err,tokens){
-
-  if(err){
-    console.log(err);
-    return;
-  } else{
-    console.log('Connected!');
-    gsrun(client)
-  }
-
-});
 
 async function gsrun(cl){
   const gsapi = google.sheets({version:'v4', auth: cl});
@@ -155,6 +135,31 @@ app.get('/webhook', function (req, res) {
     console.error("Failed validation. Make sure the validation tokens match.");
     res.sendStatus(403);
   }
+});
+
+app.get("/notihook", function (req, res) {
+
+const client = new google.auth.JWT(
+
+  keys.client_email,
+  null,
+  keys.private_key,
+  ['https://www.googleapis.com/auth/spreadsheets']
+
+);
+////////////////////////////////////// ส่วนของ Google Sheet
+client.authorize(function(err,tokens){
+
+  if(err){
+    console.log(err);
+    return;
+  } else{
+    console.log('Connected!');
+    gsrun(client)
+  }
+
+});
+
 });
 
 
