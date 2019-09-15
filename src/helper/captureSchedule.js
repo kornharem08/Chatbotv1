20d613a1d74aa2760f93bf4a41c6451eb1a71454
soapsabee <rattana152513@gmail.com>
2019-09-16 00:13:53 +0700

    test_upload_schedule

--- src/helper/captureSchedule.js
+++ src/helper/captureSchedule.js
@@ -19,34 +19,34 @@ async function captureInit() {
     });
     const page = await browser.newPage();
     await page.goto('https://webviews-vue1.herokuapp.com/Schedule');
-    await page.screenshot({ path: './scheduleTemp/schedule.png' });
-    // let fileReaderStream = await fs.createReadStream('./scheduleTemp/schedule.png')
-    // let formData = await {
-    //     "recipient": JSON.stringify({
-    //         "id": "2797221146971020"
-    //     }),
-    //     "message": JSON.stringify({
-    //         "attachment": {
-    //             "type": "image",
-    //             "payload": {
-    //                 "is_reusable": true
-    //             }
-    //         }
-    //     }),
-    //     "filedata": fileReaderStream
+    await page.screenshot({ path: 'schedule.png' });
+    let fileReaderStream = await fs.createReadStream('./schedule.png')
+    let formData = await {
+        "recipient": JSON.stringify({
+            "id": "2797221146971020"
+        }),
+        "message": JSON.stringify({
+            "attachment": {
+                "type": "image",
+                "payload": {
+                    "is_reusable": true
+                }
+            }
+        }),
+        "filedata": fileReaderStream
 
-    // }
+    }
 
-    // await request({
-    //     "method": 'POST',
-    //     "json": true,
-    //     "formData": formData,
-    //     "uri": 'https://graph.facebook.com/v4.0/me/message_attachments?access_token=EAAD4BB3LHCIBALN3oGRT2f190z6NVkzSglLZBt4nZBUgXrZBoifnZByEKs9zUkCzT1UYWdWYTGFlOaSrcL8nEfuWEArICIxQZAghZCjjiG1C0pTvkxj4yXhvF3E2lmQ7b4ZBGhbyEhGIRSVySTNr7Um4dhm1HNAreWX7o1ny3h5RKIrPy4XxM60'
-    // },
-    //     function (err, res, body) {
-    //         //***
-    //         console.log("res_attachment_id_test:" + res.body.attachment_id)
-    //     });
+    await request({
+        "method": 'POST',
+        "json": true,
+        "formData": formData,
+        "uri": 'https://graph.facebook.com/v4.0/me/message_attachments?access_token=EAAD4BB3LHCIBALN3oGRT2f190z6NVkzSglLZBt4nZBUgXrZBoifnZByEKs9zUkCzT1UYWdWYTGFlOaSrcL8nEfuWEArICIxQZAghZCjjiG1C0pTvkxj4yXhvF3E2lmQ7b4ZBGhbyEhGIRSVySTNr7Um4dhm1HNAreWX7o1ny3h5RKIrPy4XxM60'
+    },
+        function (err, res, body) {
+            //***
+            console.log("res_attachment_id_test:" + res.body.attachment_id)
+        });
 
     await browser.close();
 }
