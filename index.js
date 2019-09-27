8b6470a4294770a293c2420b9c7594202521b4c1
soapsabee <patiphan.soap@gmail.com>
2019-09-27 21:00:52 +0700

    console_log_examNotification

--- index.js
+++ index.js
@@ -414,7 +414,7 @@ const examNotification = async () =>{
           let To = examtime[indexE].To
           let SubjectNameEN = examtime[indexE].SubjectNameEN
           let ExamRoom = examtime[indexE].ExamRooms[0]
-          console.log("ExamRoom:"+examtime[indexE].ExamRooms[0])
+          console.log("ExamRoom:"+examtime[indexE].ExamRooms)
            message = `ทดสอบการแจ้งเตือน: วันนี้มีสอบนะครับ เริ่มสอบ ${From} ถึง ${To} วิชา ${SubjectNameEN} ห้อง ${ExamRoom}`   
           // func.sendTextMessage(studentid[index].senderId, message)
         }
