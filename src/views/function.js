902f79340776f939eee71c19f3bce3551e3b3442
soapsabee <patiphan.soap@gmail.com>
2019-09-30 17:55:13 +0700

    test dictionary

--- src/views/function.js
+++ src/views/function.js
@@ -154,7 +154,7 @@ const authenticated = async (sender) => {
 }
 
 const messageLanguage = async (sender) => {
-  let replies = fb.quickreplyTemplate("change language", [fb.quickreply("TH", "test", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/%E0%B8%98%E0%B8%87%E0%B8%8A%E0%B8%B2%E0%B8%95%E0%B8%B4%E0%B9%84%E0%B8%97%E0%B8%A2.png/320px-%E0%B8%98%E0%B8%87%E0%B8%8A%E0%B8%B2%E0%B8%95%E0%B8%B4%E0%B9%84%E0%B8%97%E0%B8%A2.png"), fb.quickreply("ENG", "test", null)])
+  let replies = fb.quickreplyTemplate("change language", [fb.quickreply("TH", "test" , "http://example.com/img/red.png"), fb.quickreply("ENG", "test", "http://example.com/img/red.png")])
   await sendQuickReply(sender, replies);
 }
 
