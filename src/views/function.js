0805914ccda53558a3d41b65a51d28928bdfb9b8
soapsabee <patiphan.soap@gmail.com>
2019-10-06 10:39:18 +0700

    + please wait text before capture schedule

--- src/views/function.js
+++ src/views/function.js
@@ -111,7 +111,7 @@ const btnExScheduleView = async (sender) => {
 
 }
 const messageClassSc = async (sender) => {
-  let Message = "รอแปป.."
+  let Message = "โปรดรอสักครู่.."
   await sendTextMessage(sender, Message)
   //let replies = fb.quickreplyTemplate("Please select Semester/Year criteria", [fb.quickreply("<< Back", "MainMenu_Payload", null), fb.quickreply("1/2559", "test", null), fb.quickreply("2/2559", "test", null), fb.quickreply("1/2560", "test", null), fb.quickreply("2/2560", "test", null), fb.quickreply("1/2561", "test", null), fb.quickreply("2/2561", "test", null)])
   let capture = await captureSchedule.captureInit();
