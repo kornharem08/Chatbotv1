aaf1aecef96fd3fb1b015d4d510804e64fc8b58e
soapsabee <patiphan.soap@gmail.com>
2019-10-30 18:25:09 +0700

    test array quick reply

--- src/views/function.js
+++ src/views/function.js
@@ -65,7 +65,8 @@ const quickreplyGradeGPAsemester = async (sender) => {
   // let quickreply = await setQuickreplyforgrade(sender)
   // console.log("setQuickreplyforgrade:" + quickreply[0])
   let txt_lang = await api.requestLang(sender)
-  let replies = fb.quickreplyTemplate(Dict.semester_TXT[txt_lang], [fb.quickreply(Dict.back_TXT[txt_lang], '{"campagin":"MainMenu_Payload","data":"null"}', null),fb.quickreply("1/2561", '{"campagin":"Grade_Semester_quickreply","data":"1/2561"}',null),fb.quickreply("2/2561", '{"campagin":"Grade_Semester_quickreply","data":"2/2561"}', null),fb.quickreply("1/2560", '{"campagin":"Grade_Semester_quickreply","data":"1/2560"}', null),fb.quickreply("2/2560", '{"campagin":"Grade_Semester_quickreply","data":"2/2560"}',null),fb.quickreply("1/2559", '{"campagin":"Grade_Semester_quickreply","data":"1/2559"}', null),fb.quickreply("2/2559", '{"campagin":"Grade_Semester_quickreply","data":"2/2559"}', null)])
+  let testQuickreply = [fb.quickreply(Dict.back_TXT[txt_lang], '{"campagin":"MainMenu_Payload","data":"null"}', null),fb.quickreply("1/2561", '{"campagin":"Grade_Semester_quickreply","data":"1/2561"}',null),fb.quickreply("2/2561", '{"campagin":"Grade_Semester_quickreply","data":"2/2561"}', null),fb.quickreply("1/2560", '{"campagin":"Grade_Semester_quickreply","data":"1/2560"}', null),fb.quickreply("2/2560", '{"campagin":"Grade_Semester_quickreply","data":"2/2560"}',null),fb.quickreply("1/2559", '{"campagin":"Grade_Semester_quickreply","data":"1/2559"}', null),fb.quickreply("2/2559", '{"campagin":"Grade_Semester_quickreply","data":"2/2559"}', null)]
+  let replies = fb.quickreplyTemplate(Dict.semester_TXT[txt_lang], testQuickreply)
   // console.log("quickreplyGradeGPAsemester:"+JSON.stringify(replies))
   await sendQuickReply(sender, replies);
 }
