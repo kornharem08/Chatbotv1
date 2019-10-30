dfa18b3ec5a7b8db61d25dd1e0bf7c10fc048d60
soapsabee <patiphan.soap@gmail.com>
2019-10-31 00:09:18 +0700

    test array quick reply

--- index.js
+++ index.js
@@ -275,8 +275,8 @@ app.post("/webhook/", async function (req, res) {
 
         if (messagingEvent.postback) {
           receivedPostback(messagingEvent)
-        } else if (messagingEvent.message.quick_reply) {
-          console.log("Quick-Reply:"+messagingEvent.message.quick_reply)
+        } else if (messagingEvent.message.quick_replies) {
+          console.log("Quick-Reply:"+messagingEvent.message.quick_replies)
           //let qr = JSON.parse(messagingEvent.message.quick_reply.payload)
           //console.log("Quick-Reply" + qr.campagin)
           /// ต้องทำ session อีกทีนึง
