3d998775bffa0966fc54efd0f79c0d4c82fd22df
soapsabee <patiphan.soap@gmail.com>
2019-12-01 18:22:36 +0700

    test new Schedule

--- src/helper/captureSchedule.js
+++ src/helper/captureSchedule.js
@@ -20,8 +20,13 @@ async function captureInit() {
         }
     });
     const page = await browser.newPage();
-    await page.goto('http://nuxtsiscon.herokuapp.com/Schedule/5930213055');
-    await page.waitForNavigation();
+    await page.goto('http://nuxtsiscon.herokuapp.com/Schedule/5930213055'{
+        waitUntil: 'networkidle0',
+    });
+    await page.waitForNavigation({
+        waitUntil: 'networkidle0',
+      });
+      
     await page.screenshot({ path: 'schedule.png' });
     let fileReaderStream = await fs.createReadStream('./schedule.png')
     let formData = await {
