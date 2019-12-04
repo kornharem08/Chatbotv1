afd69bf436e59669e69f66e4ea8efaf35071ed35
soapsabee <patiphan.soap@gmail.com>
2019-12-04 08:54:52 +0700

    comment handleMessageAttachments

--- index.js
+++ index.js
@@ -66,6 +66,9 @@ async function checknotiDate(dataArray) {
   if (month < 10) {
     month = `0${month}`
   }
+  if(date < 10){
+    date = `0${date}`
+  }
   let year = d.getFullYear()
   let today = `${date}/${month}/${year}`
   
