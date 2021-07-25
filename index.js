27b54e7dd70da4f23c5a00fd62f47a787653226c
soapsabee-bc <patiphan.soap@bcircle.co.th>
2021-07-25 21:02:38 +0700

    add buildpack a

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const projectId = 'botj-jldh'
 
 const credentials = {
   client_email: "sisbot@botj-jldh.iam.gserviceaccount.com",
-  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDT0TzoWY8k0+3x\nY5+GLgYjkPTxezPpU6bMFP2A1osqR73I4H6sysGKjhl4D8ylaR9bRQ0PRa/IBHzn\nQi3Y5taAmc8PSG1UQy35hPVidw77D72KwFQKHeb/BPtZZHPx2s4btEpcpaE8KvCH\n/hGArA23n5x4SW9DZjGD6C+Ys4kywYrbilNFCbhhc088Qi8c3D7S7LOEAu5i5Ef4\nySGV6NROT/6KCsmeZCp1EMU/JeCDVprG68KQHBu7NT3Y8+ckJqV4HXbWzggcpLpe\n6yGmzwOG7Hu+K86KfgGbl+Z1yRYIuZw+XHG/KjYj0ctTyVl5hHn9O8ZgNppU93b4\n9Q8eu7HxAgMBAAECggEAWWScYmjtpvrEGFRVjlDdKWRzY3h9+5Jgug9/v0CMQf8+\nAslv3KfDT6DR8eAlG4gGgkPdk5zy5ySxu/2rkLc3PtXq04XbEi05+WzaDMC3c1B8\ny1KJBS4Cf40QMPVak28m/f00Ru5l4hkzIwmgRGAYd+7mH8PKL7Uw33z9nWHj3KWZ\n1ya8UYIfciWayKv31YuBT0GPRwx676jBpXM0a88g+3LwT+TCi82sc8E9csyBN7Ph\nbEgQXpm+V7XepZphMOHOz7X+fsYC55aTSZP9nJ7DiqL9UgaTs/95U+68lu+YC1sm\nAcfYdB03UZ5hny/1dMi072LqANMmRWJXIMZuasTH2wKBgQD5sMPZrF2VPQ9hkayX\n6s5duLLXRx2DBAd88V9cMdl/48V5sRv4hpds5oUSgS33E+P4ZsYjj0VFOOaNhxK2\ngXnXwovndG+4OYrgj7moqNDQ/tShg4xeRUFHt+VxMfNds1TOpQ9T4fOTOGuIzfg/\ntpwq0AamyqHfeiNfBa62bY9wLwKBgQDZK3kwluVWlBVk34ZdZwyp3Jtrj+PZx/Ie\nv/guXo++8ccKMkqy+DU3iMg/ViIeLpGZJDdUoJG1EYUAtfrzXCj+zmFi/JGs13Ua\ntr3bfHNiA7ZtSLvwg0Ig42LtiYKowII6eF20lcRKo8gB7k3csu9m20bthbvVo/8W\nHKlLD6dX3wKBgQDHXn4zS2753+RtKq0eLE2VBgo+3LjSyR/VgsF60w7xEAS481L7\nMTStrq7opJ74HzizOhTRhe9wo0tsDEND2MRaJCwjppQiDEg4oGpFIm6Bwkgzot8s\n0q2aP1sD9Dd3RN7dlWxUtSERF0a92IB1FzFA+hOlkDyKPXMKx3XGklqq3wKBgCGC\nE7Drl3dEYgiudIz02X9cCp8OFJoFv9X+yMxeys8rfwqTt/LlWVC4Si17nMgLZ2u5\nqs/j9pd5nIv/iDkEf07RiTU8wz1oASmeCK1VBniZGgzCrDzwA8QPUbRVA1QGYBGl\ngM/cS+04WCP0IQ9sjIPSUZ9UgybzpuhCg9yRDcEBAoGBAPlrRXEbYX9b4iZohM+e\nRdSpfY/MCu86/VAd2zwc2/McID+OiXylsuVjqERC13U7DkU5zmZ66dleF63lwMzH\ntrmc8AwVn6U+JJZ8ich2Jk5oK/vqopApWuQP5Rb28GmvoOIRwbKAIJ2nyXePPKyP\niUSOPmoko8iBR0Wjt4FLnGAl\n-----END PRIVATE KEY-----\n",
+  private_key:"-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDT0TzoWY8k0+3x\nY5+GLgYjkPTxezPpU6bMFP2A1osqR73I4H6sysGKjhl4D8ylaR9bRQ0PRa/IBHzn\nQi3Y5taAmc8PSG1UQy35hPVidw77D72KwFQKHeb/BPtZZHPx2s4btEpcpaE8KvCH\n/hGArA23n5x4SW9DZjGD6C+Ys4kywYrbilNFCbhhc088Qi8c3D7S7LOEAu5i5Ef4\nySGV6NROT/6KCsmeZCp1EMU/JeCDVprG68KQHBu7NT3Y8+ckJqV4HXbWzggcpLpe\n6yGmzwOG7Hu+K86KfgGbl+Z1yRYIuZw+XHG/KjYj0ctTyVl5hHn9O8ZgNppU93b4\n9Q8eu7HxAgMBAAECggEAWWScYmjtpvrEGFRVjlDdKWRzY3h9+5Jgug9/v0CMQf8+\nAslv3KfDT6DR8eAlG4gGgkPdk5zy5ySxu/2rkLc3PtXq04XbEi05+WzaDMC3c1B8\ny1KJBS4Cf40QMPVak28m/f00Ru5l4hkzIwmgRGAYd+7mH8PKL7Uw33z9nWHj3KWZ\n1ya8UYIfciWayKv31YuBT0GPRwx676jBpXM0a88g+3LwT+TCi82sc8E9csyBN7Ph\nbEgQXpm+V7XepZphMOHOz7X+fsYC55aTSZP9nJ7DiqL9UgaTs/95U+68lu+YC1sm\nAcfYdB03UZ5hny/1dMi072LqANMmRWJXIMZuasTH2wKBgQD5sMPZrF2VPQ9hkayX\n6s5duLLXRx2DBAd88V9cMdl/48V5sRv4hpds5oUSgS33E+P4ZsYjj0VFOOaNhxK2\ngXnXwovndG+4OYrgj7moqNDQ/tShg4xeRUFHt+VxMfNds1TOpQ9T4fOTOGuIzfg/\ntpwq0AamyqHfeiNfBa62bY9wLwKBgQDZK3kwluVWlBVk34ZdZwyp3Jtrj+PZx/Ie\nv/guXo++8ccKMkqy+DU3iMg/ViIeLpGZJDdUoJG1EYUAtfrzXCj+zmFi/JGs13Ua\ntr3bfHNiA7ZtSLvwg0Ig42LtiYKowII6eF20lcRKo8gB7k3csu9m20bthbvVo/8W\nHKlLD6dX3wKBgQDHXn4zS2753+RtKq0eLE2VBgo+3LjSyR/VgsF60w7xEAS481L7\nMTStrq7opJ74HzizOhTRhe9wo0tsDEND2MRaJCwjppQiDEg4oGpFIm6Bwkgzot8s\n0q2aP1sD9Dd3RN7dlWxUtSERF0a92IB1FzFA+hOlkDyKPXMKx3XGklqq3wKBgCGC\nE7Drl3dEYgiudIz02X9cCp8OFJoFv9X+yMxeys8rfwqTt/LlWVC4Si17nMgLZ2u5\nqs/j9pd5nIv/iDkEf07RiTU8wz1oASmeCK1VBniZGgzCrDzwA8QPUbRVA1QGYBGl\ngM/cS+04WCP0IQ9sjIPSUZ9UgybzpuhCg9yRDcEBAoGBAPlrRXEbYX9b4iZohM+e\nRdSpfY/MCu86/VAd2zwc2/McID+OiXylsuVjqERC13U7DkU5zmZ66dleF63lwMzH\ntrmc8AwVn6U+JJZ8ich2Jk5oK/vqopApWuQP5Rb28GmvoOIRwbKAIJ2nyXePPKyP\niUSOPmoko8iBR0Wjt4FLnGAl\n-----END PRIVATE KEY-----\n",
 };
 // const request = require('request')
 const app = express();
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 var allowedOrigins = ['http://localhost:8080',
-  'https://webviews-vue1.herokuapp.com', 'https://nuxtsiscon.herokuapp.com'];
+  'https://webviews-vue1.herokuapp.com','https://nuxtsiscon.herokuapp.com'];
 app.use(cors({
   origin: function (origin, callback) {
     // allow requests with no origin 
@@ -56,7 +56,7 @@ async function gsrun(cl) {
   };
   let data = await gsapi.spreadsheets.values.get(opt);
   let dataArray = data.data.values
-  console.log("dataArray:" + dataArray)
+  console.log("dataArray:"+dataArray)
   // let newDataArray = dataArray.map(function(r){
   //   //console.log("datafromGoogleSheet:"+r[0])
   //   return r
@@ -73,19 +73,19 @@ async function checknotiDate(dataArray) {
   if (month < 10) {
     month = `0${month}`
   }
-  if (date < 10) {
+  if(date < 10){
     date = `0${date}`
   }
   let year = d.getFullYear()
   let today = `${date}/${month}/${year}`
-
+  
   for (let i = 0; i < dataArray.length; i++) {
 
     if (dataArray[i][1] == today) {
       sendnotiMessage(dataArray[i][0])
-
+    
     }
-
+    
 
   }
 
@@ -178,17 +178,17 @@ const receivedMessage = async (event) => {
 function receivedQuickRp(event) {
   let senderID = event.sender.id;
   let qr = event.message.quick_reply.payload
-  let postback = JSON.parse(qr)
-  console.log("receivedQuickRp:" + postback)
+  let postback = JSON.parse(qr)  
+  console.log("receivedQuickRp:"+postback)
   let value
   let campagin
 
-  if (postback.data) {
-    value = postback.data
+  if(postback.data){
+  value = postback.data
   }
 
-  if (postback.campagin) {
-    campagin = postback.campagin
+  if(postback.campagin){
+  campagin = postback.campagin
   }
   // var title = postback.title
   // var payload = postback.payload
@@ -244,38 +244,15 @@ function handlePostback(senderID, postback, value) {
 }
 
 
-// const sessionClient = new apiai.SessionsClient({
-//   credentials:credentials,
-// });
-
-const googleAuth = async (sender,text) => {
-  const aiClient = new google.auth.JWT(
-
-    credentials.client_email,
-    null,
-    credentials.private_key,
-    ['https://www.googleapis.com/auth/cloud-platform']
-
-  );
-  ////////////////////////////////////// ส่วนของ Google Sheet
-  await aiClient.authorize(function (err, tokens) {
-
-    if (err) {
-      console.log(err);
-      return;
-    } else {
-      console.log('Connected!');
-      let sessionClient = new apiai.SessionsClient();
-      aiDetect(sessionClient,sender,text)
-    }
-
+ const sessionClient = new apiai.SessionsClient({
+    credentials:credentials,
   });
-}
 
-const aiDetect = async (sessionClient,sender,text) => {
-
-
-  const sessionPath = sessionClient.sessionPath(projectId, sessionIds.get(sender));
+const sendToApiAi = async (sender, text) => {
+  sendTypingOn(sender);
+   // Create a new session
+  
+   const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionIds.get(sender));
   let apiaiRequest = {
     session: sessionPath,
     queryInput: {
@@ -288,28 +265,18 @@ const aiDetect = async (sessionClient,sender,text) => {
     },
   }
 
-  // Send request and log result
-  const responses = await sessionClient.detectIntent(apiaiRequest);
-  console.log('Detected intent');
-  const result = responses[0].queryResult;
-  console.log(`  Query: ${result.queryText}`);
-  console.log(`  Response: ${result.fulfillmentText}`);
-  if (result.intent) {
-    console.log(`  Intent: ${result.intent.displayName}`);
-  } else {
-    console.log(`  No intent matched.`);
-  }
-
-}
-
-const sendToApiAi = async (sender, text) => {
-  sendTypingOn(sender);
-  // Create a new session
-
-  googleAuth(sender,text)
-
-
-  ///
+    // Send request and log result
+    const responses = await sessionClient.detectIntent(apiaiRequest);
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+    if (result.intent) {
+      console.log(`  Intent: ${result.intent.displayName}`);
+    } else {
+      console.log(`  No intent matched.`);
+    }
+    ///
   // apiaiRequest.on("response", response => {
   //   if (isDefined(response.result)) {
   //     handleApiAiResponse(sender, response);
@@ -344,22 +311,22 @@ app.post("/webhook/", async function (req, res) {
         if (messagingEvent.postback) {
           receivedPostback(messagingEvent)
         } else if (messagingEvent.message.quick_reply) {
-          console.log("Quick-Reply:" + messagingEvent.message.quick_reply)
-          //  let qr = messagingEvent.message.quick_reply.payload
-          //  let jsonparse = JSON.parse(qr)
-          //    console.log("qr:" +jsonparse.campagin)
-
+          console.log("Quick-Reply:"+messagingEvent.message.quick_reply)
+        //  let qr = messagingEvent.message.quick_reply.payload
+        //  let jsonparse = JSON.parse(qr)
+        //    console.log("qr:" +jsonparse.campagin)
+          
           /// ต้องทำ session อีกทีนึง
-          try {
-            receivedQuickRp(messagingEvent)
-          } catch {
+          try{
+          receivedQuickRp(messagingEvent)
+          }catch{
             console.log("receivedQuickRp error")
-          }
+          } 
 
 
 
         } else if (messagingEvent.message) {
-          receivedMessage(messagingEvent).then(() => { }).catch((e) => console.log("messagingEvent:", e))
+           receivedMessage(messagingEvent).then(() => {}).catch((e) => console.log("messagingEvent:",e))
         } else {
           console.log("Webhook received unknown messagingEvent: ", messagingEvent);
         }
@@ -376,14 +343,14 @@ app.post("/webhook/", async function (req, res) {
       if (auth.status == "authenticate") {
         authenticate(data)
         status = 200
-      } else {
+      }else{
         status = 404
       }
 
     });
-    if (status == 200) {
+    if(status == 200){
       res.sendStatus(200);
-    } else if (status == 404) {
+    }else if (status == 404){
       res.status(404).send('Sorry, we cannot find that senderid! maybe senderid is wrong!');
     }
   }
@@ -416,7 +383,7 @@ const examNotification = async () => {
   let year = d.getFullYear() + 543
   let today = `${date}/${month}/${year}`
   let message = "วันนี้มีสอบนะจ๊ะ: "
-
+  
   await listwho.forEach(element => {
     let studentinfo = {
       senderId: element.senderId,
@@ -425,10 +392,10 @@ const examNotification = async () => {
     studentid.push(studentinfo)
   });
   let setExam = await setExamcurrently(listwho[0].senderId)
-  console.log("setExam:" + setExam.eduyear + "/" + setExam.eduterm + "/" + setExam.miniterm)
+  console.log("setExam:"+setExam.eduyear+"/"+setExam.eduterm+"/"+setExam.miniterm)
   for (let index = 0; index < studentid.length; index++) {
 
-    examtime = await api.requestTimeExam(studentid[index].studentID, setExam.eduyear, setExam.eduterm, setExam.miniterm)
+    examtime = await api.requestTimeExam(studentid[index].studentID,setExam.eduyear,setExam.eduterm,setExam.miniterm)
     for (let indexE = 0; indexE < examtime.length; indexE++) {
       if (examtime[indexE].ExamDate == today) {
         let From = examtime[indexE].From
@@ -458,47 +425,47 @@ async function setExamcurrently(senderid) {
   let lastelement
   let lastterm
   let miniterm
-  let text = []
-  for (let i = 0; i < Object.keys(eduyear).length; i++) {
-    let eduterm = groupBy(eduyear[`${Object.keys(eduyear)[i]}`], 'EduTerm')
-    let term
-    if (Object.keys(eduterm) == "1") {
-      term = 1
-    } else if (Object.keys(eduterm) == "1,2") {
-      term = 2
-    } else if (Object.keys(eduterm) == "1,2,3") {
-      term = 3
+   let text = []
+   for (let i = 0; i < Object.keys(eduyear).length; i++) {
+     let eduterm = groupBy(eduyear[`${Object.keys(eduyear)[i]}`], 'EduTerm')
+     let term
+      if(Object.keys(eduterm) == "1"){
+        term = 1
+      }else if(Object.keys(eduterm) == "1,2"){
+        term = 2
+      }else if(Object.keys(eduterm) == "1,2,3"){
+        term = 3
+      }
+       text.push({eduyear:Object.keys(eduyear)[i],eduterm:term})
+      
+   }
+  
+   lastterm = text[text.length-1].eduterm
+    
+   if(lastterm == 1 && month < 11 && month > 7 ){
+      miniterm = "M"
+    }else if(lastterm == 2 && month <= 4 && month > 1){
+      miniterm = "M"
+    }else if(lastterm == 1 && month >=11 && month < 13){
+      miniterm = "F"
+    }else if(lastterm == 2 && month > 4 && month <= 7){
+      miniterm = "F"
     }
-    text.push({ eduyear: Object.keys(eduyear)[i], eduterm: term })
 
-  }
-
-  lastterm = text[text.length - 1].eduterm
-
-  if (lastterm == 1 && month < 11 && month > 7) {
-    miniterm = "M"
-  } else if (lastterm == 2 && month <= 4 && month > 1) {
-    miniterm = "M"
-  } else if (lastterm == 1 && month >= 11 && month < 13) {
-    miniterm = "F"
-  } else if (lastterm == 2 && month > 4 && month <= 7) {
-    miniterm = "F"
-  }
-
-  lastelement = { eduyear: text[text.length - 1].eduyear, eduterm: lastterm, miniterm: miniterm }
-  return lastelement
-}
-
-function groupBy(objectArray, property) {
-  return objectArray.reduce(function (acc, obj) {
-    var key = obj[property]
-    if (!acc[key]) {
-      acc[key] = []
-    }
-    acc[key].push(obj)
-    return acc;
-  }, {})
-}
+  lastelement = { eduyear:text[text.length-1].eduyear, eduterm:lastterm, miniterm:miniterm }
+   return lastelement
+ }
+ 
+ function groupBy(objectArray, property) {
+   return objectArray.reduce(function (acc, obj) {
+     var key = obj[property]
+     if (!acc[key]) {
+       acc[key] = []
+     }
+     acc[key].push(obj)
+     return acc;
+   }, {})
+ }
 
 
 
